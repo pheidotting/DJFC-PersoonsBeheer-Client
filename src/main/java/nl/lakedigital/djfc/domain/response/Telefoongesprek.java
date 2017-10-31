e15fec848ed3359b842018eba15741b93c05caf2
Patrick Heidotting <patrick@lakedigital.nl>
2017-10-31 21:59:35 +0100

    SYMPLEX-79

--- src/main/java/nl/lakedigital/djfc/domain/response/Telefoongesprek.java
+++ src/main/java/nl/lakedigital/djfc/domain/response/Telefoongesprek.java
@@ -2,6 +2,8 @@ package nl.lakedigital.djfc.domain.response;
 
 public class Telefoongesprek {
     private String bestandsnaam;
+    private String telefoonnummer;
+    private String tijdstip;
 
     public String getBestandsnaam() {
         return bestandsnaam;
@@ -9,5 +11,21 @@ public class Telefoongesprek {
 
     public void setBestandsnaam(String bestandsnaam) {
         this.bestandsnaam = bestandsnaam;
+    }
+
+    public String getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(String telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getTijdstip() {
+        return tijdstip;
+    }
+
+    public void setTijdstip(String tijdstip) {
+        this.tijdstip = tijdstip;
     }
 }
